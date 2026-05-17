ed6714883bf93b309360b9dc6f21c50faf7821d0
Malik-Haziq <malikhaziq153@gmail.com>
2026-05-17 23:42:39 +0500

    update: add new resume link

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,4 +1,4 @@
 export const LINKS = {
   RESUME:
-    "https://drive.google.com/file/d/1IucN8mGW9E7rnTmrT8k6HMGr1KuXpvSV/view?usp=sharing",
+    "https://drive.google.com/file/d/1oS6FEIoIMVw79bJ_a4DvN5Fgeff-phQx/view?usp=sharing",
 };
